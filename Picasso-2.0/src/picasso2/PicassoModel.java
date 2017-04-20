d02a6f4ecca0d3d2664a1183c11a98b5e4164016
Paul Ferreras <Paul Ferreras>
2017-04-20 15:39:37 -0400

    added Picasso Model

---
+++ Picasso-2.0/src/picasso2/PicassoModel.java
@@ -0,0 +1,13 @@
+package picasso2;
+
+import java.awt.image.BufferedImage;
+
+public class PicassoModel {
+
+	private BufferedImage buffered_image;
+	
+	public PicassoModel() {
+		buffered_image = new BufferedImage(500, 500, BufferedImage.TYPE_3BYTE_BGR);
+	}
+
+}
