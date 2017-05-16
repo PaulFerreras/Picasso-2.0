158f8f59fd6b44535420c30ec386e20def6c6b7f
Paul Ferreras <Paul Ferreras>
2017-05-16 18:18:10 -0400

    Created PicassoToolBarListener inside of PicassoToolBar to handle Mouse and Mouse Motion Events. Made PicassoToolBar observable to PicassoButtonPanel so that PicassoButtonPanel can adapt to PicassoToolBar's orientation.

--- Picasso-2.0/src/picasso2/PicassoToolBar.java
+++ Picasso-2.0/src/picasso2/PicassoToolBar.java
@@ -1,19 +1,29 @@
 package picasso2;
 
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Dimension;
+import java.awt.event.MouseEvent;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
 import javax.swing.border.Border;
+import javax.swing.event.MouseInputAdapter;
+import javax.swing.event.MouseInputListener;
 
 /*PF: Draggable ToolBar that holds PicassoButtonPanel
  * Extends JToolBar
+ * Is Observable by PicassoButtonPanel
+ * Notifies PicassoButtonPanel when
+ * PicassoToolBar's orientation has changed
+ * Also houses PicassoToolBarListener
  */
 
-public class PicassoToolBar extends JToolBar {
-
+public class PicassoToolBar extends JToolBar implements NewObservable {
+	
+	private NewObserver observer;
+	
 	public PicassoToolBar(String name) {
 		//PF: Name gets delegated to JToolBar constructor
 		super(name);
@@ -35,8 +45,54 @@ public class PicassoToolBar extends JToolBar {
 		//PF: Add button panel to background panel
 		background_panel.add(button_panel);
 		
+		//PF: Create PicassoToolBarListener
+		PicassoToolBarListener picasso_tbl = new PicassoToolBarListener();
+		
+		/*PF: Add PicassoToolBarListener as Mouse Listener 
+		 * and Mouse Motion Listener
+		 */
+		addMouseListener(picasso_tbl);
+		addMouseMotionListener(picasso_tbl);
+		
 		//PF: Add background panel to PicassoToolBar
 		add(background_panel);
 	}
+
+
+	@Override
+	public void addObserver(NewObserver o) {
+		observer = o;
+		
+	}
+
+
+	@Override
+	public void notifyObserver() {
+		observer.update();
+		
+	}
+	
+public class PicassoToolBarListener extends MouseInputAdapter {
+	
+	public PicassoToolBarListener() {
+		
+	}
+	
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		if(e.getX() < 14) {
+			setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		} else {
+			setCursor(Cursor.getDefaultCursor());
+		}
+	}
+	
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		if(getOrientation() == JToolBar.VERTICAL) {
+			
+		}
+	}
+}
 	
 }
